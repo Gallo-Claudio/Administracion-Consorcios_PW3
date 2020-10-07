973ac6e7c3db9279560acd719dab7d72b0423618
Gallo <g>
2020-10-07 15:59:57 -0300

    Listar Consorcios

---
+++ Servicios/ConsorcioServicio.cs
@@ -0,0 +1,18 @@
+﻿using Datos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public static class ConsorcioServicio
+    {
+        public static List<Consorcio> ListarConsorcios()
+        {
+            return Hardcodeo.consorcio;
+        }
+    }
+}
---
+++ WebApp/Controllers/ConsorcioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entidades;
+using Servicios;
+
+namespace WebApp.Controllers
+{
+    public class ConsorcioController : Controller
+    {
+        // GET: Consorcio
+        public ActionResult ListarConsorcio()
+        {
+            List<Consorcio> listadoConsorcio = ConsorcioServicio.ListarConsorcios();
+            return View(listadoConsorcio);
+        }
+    }
+}
