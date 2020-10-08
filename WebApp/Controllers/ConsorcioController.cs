88cfa4e8fb54971a3ca3539795b46c7cc8e2a02f
Gallo <g>
2020-10-08 01:31:35 -0300

    Ingreso de Consorcio

--- WebApp/Controllers/ConsorcioController.cs
+++ WebApp/Controllers/ConsorcioController.cs
@@ -16,5 +16,20 @@ namespace WebApp.Controllers
             List<Consorcio> listadoConsorcio = ConsorcioServicio.ListarConsorcios();
             return View(listadoConsorcio);
         }
+        
+        public ActionResult AgregarConsorcio()
+        {
+            List<Provincia> provincias = ProvinciaServicio.ListarProvincias();
+            ViewData["listadoProvincias"] = provincias;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarConsorcio(Entidades.Consorcio consorcio, int IdProvincia)
+        {       
+            ConsorcioServicio.AgregarConsorcio(consorcio, IdProvincia);
+            List<Entidades.Consorcio> listadoConsorcio = ConsorcioServicio.ListarConsorcios();
+            return RedirectToAction("ListarConsorcio", listadoConsorcio);
+        }
     }
 }
