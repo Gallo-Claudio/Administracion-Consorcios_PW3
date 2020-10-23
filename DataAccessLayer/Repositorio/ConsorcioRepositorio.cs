76f2914773d6b46d5f2afabf19012d4977614569
Gallo <g>
2020-10-23 02:30:36 -0300

    Listar Consorcios (Desde BD - no harcodeado)

---
+++ DataAccessLayer/Repositorio/ConsorcioRepositorio.cs
@@ -0,0 +1,58 @@
+﻿using DataAccessLayer.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositorio
+{
+    public class ConsorcioRepositorio
+    {
+        ContextoEntities contexto = new ContextoEntities();
+
+        public void Alta(Consorcio consorcio)
+        {
+            contexto.Consorcio.Add(consorcio);
+            contexto.SaveChanges();
+        }
+
+        public List<Consorcio> ObtenerTodos()
+        {
+            List<Consorcio> todosLosConsorcios = contexto.Consorcio.ToList();
+            return todosLosConsorcios;
+        }
+
+        public Consorcio ObtenerPorId(int idConsorcio)
+        {
+            Consorcio consorcio;
+            consorcio = contexto.Consorcio.Find(idConsorcio);
+            return consorcio;
+        }
+
+        public void Eliminar(int idConsorcio)
+        {
+            Consorcio consorcio = ObtenerPorId(idConsorcio);
+            if (consorcio != null)
+            {
+                contexto.SaveChanges();
+            }
+        }
+
+        public void Modificar(Consorcio consorcio)
+        {
+            Consorcio consorcioActual = ObtenerPorId(consorcio.IdConsorcio);
+            consorcioActual.Altura = consorcio.Altura;
+            consorcioActual.Calle = consorcio.Calle;
+            consorcioActual.Ciudad = consorcio.Ciudad;
+            consorcioActual.DiaVencimientoExpensas = consorcio.DiaVencimientoExpensas;
+            consorcioActual.Gasto = consorcio.Gasto;
+            consorcioActual.IdProvincia = consorcio.IdProvincia;
+            consorcioActual.Nombre = consorcio.Nombre;
+            consorcioActual.Unidad = consorcio.Unidad;
+            consorcioActual.Provincia = consorcio.Provincia;
+
+            contexto.SaveChanges();
+        }
+    }
+}
--- Servicios/ConsorcioServicio.cs
+++ Servicios/ConsorcioServicio.cs
@@ -1,5 +1,6 @@
-﻿using Datos;
-using Entidades;
+﻿using DataAccessLayer.Modelos;
+using DataAccessLayer.Repositorio;
+using Datos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,79 +9,109 @@ using System.Threading.Tasks;
 
 namespace Servicios
 {
-    public static class ConsorcioServicio
+    public class ConsorcioServicio
     {
-        public static List<Consorcio> ListarConsorcios()
-        {
-            return Hardcodeo.consorcio;
-        }
+        ConsorcioRepositorio repositorio = new ConsorcioRepositorio();
 
-        public static void AgregarConsorcio(Consorcio nuevoConsorcio)
+        public List<Consorcio> ListarConsorcios()
         {
-            nuevoConsorcio.IdProvincia = ProvinciaServicio.BuscarProvincia(nuevoConsorcio.IdProvincia.IdProvincia);
-            //nuevoConsorcio.FechaCreacion = DateTime.Now;
-            //nuevoConsorcio.IdUsuarioCreador = Hardcodeo.usu1; // HARDCODEADO - Este valor se debe obtener de la sesion
-            nuevoConsorcio.IdConsorcio = Hardcodeo.consorcio.Last().IdConsorcio + 1;   // Este valor es un autoincremental generado en la BD
-            Hardcodeo.consorcio.Add(nuevoConsorcio);
-        }
-
-        public static Consorcio BuscarConsorcio(int id)
-        {
-            Consorcio busquedaConsorcio = Hardcodeo.consorcio.Find(c => c.IdConsorcio == id);
-            return busquedaConsorcio;
-        }
-
-        public static int BuscarIdConsorcio(Consorcio consorcio)
-        {
-            int idConsorcio = consorcio.IdConsorcio;
-            return idConsorcio;
-        }
-
-        public static void ModificarConsorcio(Consorcio consorcioModificacion)
-        {
-            int id = consorcioModificacion.IdConsorcio;
-            Consorcio edicionConsorcio = Hardcodeo.consorcio.Find(c => c.IdConsorcio == id);
-
-            edicionConsorcio.Altura = consorcioModificacion.Altura;
-            edicionConsorcio.Calle = consorcioModificacion.Calle;
-            edicionConsorcio.Ciudad = consorcioModificacion.Ciudad;
-            edicionConsorcio.DiaVencimientoExpensas = consorcioModificacion.DiaVencimientoExpensas;
-            //edicionConsorcio.FechaCreacion = consorcioModificacion.FechaCreacion;
-            edicionConsorcio.IdConsorcio = consorcioModificacion.IdConsorcio;
-            edicionConsorcio.Nombre = consorcioModificacion.Nombre;
-            //edicionConsorcio.IdUsuarioCreador = consorcioModificacion.IdUsuarioCreador;
-            edicionConsorcio.IdProvincia = ProvinciaServicio.BuscarProvincia(consorcioModificacion.IdProvincia.IdProvincia);
+            return repositorio.ObtenerTodos();
         }
 
 
-        // ***** PASARLO A GASTOSERVICIO O COMO SE TERMINE LLAMANDO *****
-        public static void EliminarGastos(Consorcio consorcioEliminar)
-        {
-            foreach (Gasto gastoEliminar in Hardcodeo.gasto)
-            {
-                if (gastoEliminar.IdConsorcio == consorcioEliminar)
-                {
-                    Hardcodeo.gasto.RemoveAt(gastoEliminar.IdGasto);
-                }
-            }
-        }
 
-        // ***** PASARLO A UNIDADSERVICIO O COMO SE TERMINE LLAMANDO *****
-        public static void EliminarUnidades(Consorcio consorcioEliminar)
-        {
-            foreach (Unidad unidadEliminar in Hardcodeo.unidad)
-            {
-                if (unidadEliminar.IdConsorcio == consorcioEliminar)
-                {
-                    Hardcodeo.unidad.RemoveAt(unidadEliminar.IdUnidad);
-                }
-            }
-        }
 
-        public static void EliminarConsorcio(Consorcio consorcioEliminar)
-        {
-            int Id = consorcioEliminar.IdConsorcio;
-            Hardcodeo.consorcio.RemoveAt(Id);
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public static List<Consorcio> ListarConsorcios()
+        //{
+        //    return Hardcodeo.consorcio;
+        //}
+
+        //public static void AgregarConsorcio(Consorcio nuevoConsorcio)
+        //{
+        //    nuevoConsorcio.IdProvincia = ProvinciaServicio.BuscarProvincia(nuevoConsorcio.IdProvincia.IdProvincia);
+        //    //nuevoConsorcio.FechaCreacion = DateTime.Now;
+        //    //nuevoConsorcio.IdUsuarioCreador = Hardcodeo.usu1; // HARDCODEADO - Este valor se debe obtener de la sesion
+        //    nuevoConsorcio.IdConsorcio = Hardcodeo.consorcio.Last().IdConsorcio + 1;   // Este valor es un autoincremental generado en la BD
+        //    Hardcodeo.consorcio.Add(nuevoConsorcio);
+        //}
+
+        //public static Consorcio BuscarConsorcio(int id)
+        //{
+        //    Consorcio busquedaConsorcio = Hardcodeo.consorcio.Find(c => c.IdConsorcio == id);
+        //    return busquedaConsorcio;
+        //}
+
+        //public static int BuscarIdConsorcio(Consorcio consorcio)
+        //{
+        //    int idConsorcio = consorcio.IdConsorcio;
+        //    return idConsorcio;
+        //}
+
+        //public static void ModificarConsorcio(Consorcio consorcioModificacion)
+        //{
+        //    int id = consorcioModificacion.IdConsorcio;
+        //    Consorcio edicionConsorcio = Hardcodeo.consorcio.Find(c => c.IdConsorcio == id);
+
+        //    edicionConsorcio.Altura = consorcioModificacion.Altura;
+        //    edicionConsorcio.Calle = consorcioModificacion.Calle;
+        //    edicionConsorcio.Ciudad = consorcioModificacion.Ciudad;
+        //    edicionConsorcio.DiaVencimientoExpensas = consorcioModificacion.DiaVencimientoExpensas;
+        //    //edicionConsorcio.FechaCreacion = consorcioModificacion.FechaCreacion;
+        //    edicionConsorcio.IdConsorcio = consorcioModificacion.IdConsorcio;
+        //    edicionConsorcio.Nombre = consorcioModificacion.Nombre;
+        //    //edicionConsorcio.IdUsuarioCreador = consorcioModificacion.IdUsuarioCreador;
+        //    edicionConsorcio.IdProvincia = ProvinciaServicio.BuscarProvincia(consorcioModificacion.IdProvincia.IdProvincia);
+        //}
+
+
+        //// ***** PASARLO A GASTOSERVICIO O COMO SE TERMINE LLAMANDO *****
+        //public static void EliminarGastos(Consorcio consorcioEliminar)
+        //{
+        //    foreach (Gasto gastoEliminar in Hardcodeo.gasto)
+        //    {
+        //        if (gastoEliminar.IdConsorcio == consorcioEliminar)
+        //        {
+        //            Hardcodeo.gasto.RemoveAt(gastoEliminar.IdGasto);
+        //        }
+        //    }
+        //}
+
+        //// ***** PASARLO A UNIDADSERVICIO O COMO SE TERMINE LLAMANDO *****
+        //public static void EliminarUnidades(Consorcio consorcioEliminar)
+        //{
+        //    foreach (Unidad unidadEliminar in Hardcodeo.unidad)
+        //    {
+        //        if (unidadEliminar.IdConsorcio == consorcioEliminar)
+        //        {
+        //            Hardcodeo.unidad.RemoveAt(unidadEliminar.IdUnidad);
+        //        }
+        //    }
+        //}
+
+        //public static void EliminarConsorcio(Consorcio consorcioEliminar)
+        //{
+        //    int Id = consorcioEliminar.IdConsorcio;
+        //    Hardcodeo.consorcio.RemoveAt(Id);
+        //}
     }
 }
