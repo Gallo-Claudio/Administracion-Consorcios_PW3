5ee1965e336992adf5a849fb83d1e9ec0d47c0a8
juampifortu <juampifortunato98@gmail.com>
2020-10-25 21:15:02 -0300

    Creo clase UnidadRepositorio, con sus métodos correspondientes
    Agrego en la clase UnidadRepositorio los metodos:
    
    Alta
    MostrarTodas
    BuscarPorId
    Eliminar
    Modificar

---
+++ DataAccessLayer/Repositorio/UnidadRepositorio.cs
@@ -0,0 +1,69 @@
+﻿using DataAccessLayer.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositorio
+{
+    public class UnidadRepositorio
+    {
+        ContextoEntities ctxUnidad;
+
+        public UnidadRepositorio(ContextoEntities contexto)
+        {
+            ctxUnidad = contexto;
+        }
+
+        public void Alta(Unidad unidad)
+        {
+            ctxUnidad.Unidad.Add(unidad);
+            ctxUnidad.SaveChanges();
+        }
+
+        public List<Unidad> MostrarTodas(int id)
+        {
+            List<Unidad> todasUnidades = (from unidadbd in ctxUnidad.Unidad
+                                          where unidadbd.IdUnidad == id
+                                          orderby unidadbd.Nombre
+                                          select unidadbd).ToList();
+            return todasUnidades;                                   
+        }
+
+        public Unidad BuscarPorId(int idUnidad)
+        {
+            Unidad unidad;
+            unidad = ctxUnidad.Unidad.Find(idUnidad);
+
+            return unidad;
+        }
+
+        public void Eliminar(int idUnidad)
+        {
+            Unidad unidad;
+            unidad = BuscarPorId(idUnidad);
+
+            if(unidad != null)
+            {
+                ctxUnidad.Unidad.Remove(unidad);
+                ctxUnidad.SaveChanges();
+            }
+        }
+
+        public void Modificar(Unidad unidad)
+        {
+            Unidad unidadActual;
+
+            unidadActual = BuscarPorId(unidad.IdUnidad);
+            unidadActual.Nombre = unidad.Nombre;
+            unidadActual.NombrePropietario = unidad.NombrePropietario;
+            unidadActual.ApellidoPropietario = unidad.ApellidoPropietario;
+            unidadActual.EmailPropietario = unidad.EmailPropietario;
+            unidadActual.Superficie = unidad.Superficie;
+
+            ctxUnidad.SaveChanges();
+
+         }
+    }
+}
