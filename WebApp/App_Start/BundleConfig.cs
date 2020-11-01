ab57295bd10d91a3abe238fd4613f92e501ab498
Gallo <g>
2020-11-01 20:43:56 -0300

    Se agregan los Bundles - Reordenamiento de codigo en vistas
    Corrección del mensaje mostrado por SweetAlert

---
+++ WebApp/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebApp.App_Start
+{
+    public class BundleConfig
+    {
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.*",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dataTables").Include(
+                        "~/Scripts/DTables/datatables.min.js",
+                        "~/Scripts/DTables/ListadoTabla.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/sweetAlert").Include(
+                        "~/Scripts/sweetAlert/sweetalert2@10.js",
+                        "~/Scripts/sweetAlert/promise-polyfill.js"));
+
+
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css"));
+            
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
