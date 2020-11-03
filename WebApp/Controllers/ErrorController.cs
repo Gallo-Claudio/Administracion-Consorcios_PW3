421d819a2f210f06d1ae973a2042014b2dad1f4a
Gallo <g>
2020-11-03 18:26:50 -0300

    Pagina de Error - Se implementa la captura de errores 404 / 500

---
+++ WebApp/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Inicio(int error = 0)
+        {
+            switch (error)
+            {
+                case 404:
+                    ViewBag.Title = "Ocurrio un error inesperado";
+                    ViewBag.DescripcionError = "Esto es muy vergonzoso, esperemos que no vuelva a pasar ..";
+                    break;
+
+                case 500:
+                    ViewBag.Title = "Página no encontrada";
+                    ViewBag.DescripcionError = "La URL que está intentando ingresar no existe";
+                    break;
+
+                default:
+                    ViewBag.Title = "Página no encontrada";
+                    ViewBag.DescripcionError = "Algo salio muy mal :( ..";
+                    break;
+            }
+
+            return View("~/views/error/PaginaError.cshtml");
+        }
+    }
+}
