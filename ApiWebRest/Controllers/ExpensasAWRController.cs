541012bd6e1c897ed3dbc3b9e35c82f0ddd68182
Gallo <g>
2020-11-11 22:20:49 -0300

    Se obtiene el detalle de expensas por Api Web Rest

---
+++ ApiWebRest/Controllers/ExpensasAWRController.cs
@@ -0,0 +1,42 @@
+﻿using ApiWebRest.Models;
+using AutoMapper;
+using DataAccessLayer.Modelos;
+using Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiWebRest.Controllers
+{
+    public class ExpensasAWRController : ApiController
+    {
+        MapperConfiguration mapconfig;
+        ContextoEntities contexto = new ContextoEntities();
+
+        public ExpensasAWRController()
+        {
+            mapconfig = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<DataAccessLayer.Modelos.sp_Expensas_Result, ExpensasDTO>();
+            });
+        }
+
+        public IEnumerable<ExpensasDTO> Get(int id)
+        {
+            IMapper iMapper = mapconfig.CreateMapper();
+
+            List<ExpensasDTO> listadoDTO = new List<ExpensasDTO>();
+
+            foreach (sp_Expensas_Result expensa in contexto.sp_Expensas(id))
+            {
+                ExpensasDTO dto = iMapper.Map<DataAccessLayer.Modelos.sp_Expensas_Result, ExpensasDTO>(expensa);
+                listadoDTO.Add(dto);
+            }
+
+            return listadoDTO;
+        }
+    }
+}
---
+++ ApiWebRest/Models/ExpensasDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiWebRest.Models
+{
+    public class ExpensasDTO
+    {        
+        public int Año { get; set; }
+        public int Mes { get; set; }
+        public decimal Gasto_Total { get; set; }
+    }
+}
