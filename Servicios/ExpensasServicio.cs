086164802fe2e060e9a7c1932dd92686dbc7e5a3
Gallo <g>
2020-11-13 10:43:08 -0300

    Terminación Expensas y ajustes menores
    - Breadcrumb Expensas
    - Ajustes de datos en la vista Expensas
    - Determinación mes actual Expensas
    - Se agrega cantidad unidades en Modificar Consorcio

--- Servicios/ExpensasServicio.cs
+++ Servicios/ExpensasServicio.cs
@@ -12,9 +12,15 @@ namespace Servicios
 {
     public class ExpensasServicio
     {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Obtiene las expensasa a traves de una Web APi Rest</returns>
         public List<sp_Expensas_Result> GetExpensas(int id)
         {
-            var url = $"http://localhost:49169/api/ExpensasAWR/"+id;
+            var url = $"http://localhost:49169/api/ExpensasAWR/" + id;
             var request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "GET";
             request.ContentType = "application/json";
@@ -32,6 +38,30 @@ namespace Servicios
                         return result;
                     }
                 }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expensas"></param>
+        /// <returns>
+        /// Chequea si hay un registro con el año y mes actual. Si no hay lo agrega
+        /// </returns>
+        public List<sp_Expensas_Result> DeterminaMesActual(List<sp_Expensas_Result> expensas)
+        {
+            if (DateTime.Now.Year == expensas[0].Año && DateTime.Now.Month == expensas[0].Mes)
+            {
+                return expensas;
+            }
+            else
+            {
+                sp_Expensas_Result mesActual = new sp_Expensas_Result();
+                mesActual.Gasto_Total = 0;
+
+                expensas.Insert(0, mesActual);
+
+                return expensas;
             }
         }
     }
