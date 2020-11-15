c0d178d907fe02ca57a7a96f33db75c856f67aca
Alejo Zonta <alejozonta99@gmail.com>
2020-11-14 21:18:53 -0300

    Se agrega TipoGastoRepo

---
+++ DataAccessLayer/Repositorio/TipoGastoRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositorio
+{
+    public class TipoGastoRepositorio
+    {
+        ContextoEntities ctxTipoGasto;
+
+        public TipoGastoRepositorio(ContextoEntities contexto)
+        {
+            ctxTipoGasto = contexto;
+        }
+
+        public List<TipoGasto> ObtenerTodos()
+        {
+            List<TipoGasto> todosLosTiposGastos = (from tipogastobd in ctxTipoGasto.TipoGasto
+                                                   select tipogastobd).ToList();
+            return todosLosTiposGastos;
+        }
+
+        public TipoGasto ObtenerPorId(int id)
+        {
+            TipoGasto tipoGasto;
+            tipoGasto = ctxTipoGasto.TipoGasto.Find(id);
+            return tipoGasto;
+        }
+    }
+}
