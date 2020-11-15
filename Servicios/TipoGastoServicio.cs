6479a5a7645ac7a58519b020596bfd5ddf79fd2d
Alejo Zonta <alejozonta99@gmail.com>
2020-11-14 21:18:33 -0300

    Se agrega TipoGastoServicio

---
+++ Servicios/TipoGastoServicio.cs
@@ -0,0 +1,29 @@
+﻿using DataAccessLayer.Modelos;
+using DataAccessLayer.Repositorio;
+using System.Collections.Generic;
+
+namespace Servicios
+{
+    public class TipoGastoServicio
+    {
+        TipoGastoRepositorio repoTipoGasto;
+
+        public TipoGastoServicio(ContextoEntities contexto)
+        {
+            ContextoEntities ctx = contexto;
+
+            repoTipoGasto = new TipoGastoRepositorio(ctx);
+        }
+        public List<TipoGasto> ListarTipoGastos()
+        {
+            return repoTipoGasto.ObtenerTodos();
+        }
+
+        public TipoGasto BuscarUnidad(int id)
+        {
+            TipoGasto tipoGasto = repoTipoGasto.ObtenerPorId(id);
+            return tipoGasto;
+        }
+
+    }
+}
