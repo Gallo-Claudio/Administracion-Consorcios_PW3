a1483c07422e1bbd7382d828763e7b5f768143ee
Alejo Zonta <alejozonta99@gmail.com>
2020-11-23 20:09:21 -0300

    Se agrega Unidad Controller

---
+++ WebApp/Controllers/UnidadController.cs
@@ -0,0 +1,189 @@
+﻿using DataAccessLayer.Modelos;
+using MvcSiteMapProvider;
+using Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class UnidadController : Controller
+    {
+        ConsorcioServicio consorcio;
+        UnidadServicio unidad;
+
+        public UnidadController()
+        {
+            ContextoEntities contexto = new ContextoEntities();
+            consorcio = new ConsorcioServicio(contexto);
+            unidad = new UnidadServicio(contexto);
+        }
+
+        public ActionResult ListarUnidades(int id)
+        {
+            if (Session["IdUsuario"] != null)
+            {
+                Consorcio busquedaConsorcio = consorcio.BuscarConsorcio(id);
+                var node = SiteMaps.Current.CurrentNode;
+                if (node != null && node.ParentNode != null)
+                {
+                    node.ParentNode.Title = "Consorcio \"" + busquedaConsorcio.Nombre + "\"";
+                }
+
+                ViewBag.idConsorcio = id;
+                List<Unidad> listadoUnidades = unidad.ListarUnidades(id);
+                return View(listadoUnidades);
+            }
+            else
+            {
+                TempData["Controlador"] = "Unidad";
+                TempData["Accion"] = "Listarunidades";
+                return RedirectToAction("Ingresar", "Home");
+            }
+        }
+
+
+        public ActionResult AgregarUnidad(int id)
+        {
+            if (Session["IdUsuario"] != null)
+            {
+                Consorcio busquedaConsorcio = consorcio.BuscarConsorcio(id);
+                var node = SiteMaps.Current.CurrentNode;
+                if (node != null && node.ParentNode != null)
+                {
+                    node.ParentNode.ParentNode.Title = "Consorcio \"" + busquedaConsorcio.Nombre + "\"";
+                }
+
+                ViewData["Consorcio"] = busquedaConsorcio;
+                return View();
+            }
+            else
+            {
+                TempData["Controlador"] = "Unidad";
+                TempData["Accion"] = "AgregarUnidad";
+                return RedirectToAction("Ingresar", "Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AgregarUnidad(Unidad nuevaUnidad, int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    unidad.AgregarUnidades(nuevaUnidad, Session["IdUsuario"]);
+                }
+                catch
+                {
+                    TempData["error"] = "No se pudo guardar el registro";
+                }
+
+            }
+            else
+            {
+                ViewData["Consorcio"] = consorcio.BuscarConsorcio(nuevaUnidad.IdConsorcio);
+                return View(nuevaUnidad);
+            }
+
+            TempData["nombreUnidad"] = nuevaUnidad.Nombre;
+            switch (id)
+            {
+                case 1:
+                    ViewData["Consorcio"] = consorcio.BuscarConsorcio(nuevaUnidad.IdConsorcio);
+                    return RedirectToAction("AgregarUnidad");
+
+                default:
+                    string url = "/unidad/ListarUnidades/" + nuevaUnidad.IdConsorcio;
+                    return Redirect(url);
+            }
+        }
+
+        public ActionResult ModificarUnidad(int id)
+        {
+            if (Session["IdUsuario"] != null)
+            {
+                Unidad unidadFuncional = unidad.BuscarUnidad(id);
+
+                Consorcio busquedaConsorcio = consorcio.BuscarConsorcio(unidadFuncional.IdConsorcio);
+                var node = SiteMaps.Current.CurrentNode;
+                if (node != null && node.ParentNode != null)
+                {
+                    node.ParentNode.ParentNode.Title = "Consorcio \"" + busquedaConsorcio.Nombre + "\"";
+                }
+
+                ViewData["Consorcio"] = busquedaConsorcio;
+
+                return View(unidadFuncional);
+            }
+            else
+            {
+                TempData["Controlador"] = "Unidad";
+                TempData["Accion"] = "ModificarUnidad";
+                return RedirectToAction("Ingresar", "Home");
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult ModificarUnidad(Unidad unidadModificado)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    unidad.ModificarUnidad(unidadModificado);
+                }
+                catch
+                {
+                    TempData["error"] = "No se pudo modificar el registro";
+
+                    ViewData["Consorcio"] = consorcio.BuscarConsorcio(unidadModificado.IdConsorcio);
+                    return View(unidadModificado);
+                }
+            }
+            else
+            {
+                ViewData["Consorcio"] = consorcio.BuscarConsorcio(unidadModificado.IdConsorcio);
+                return View(unidadModificado);
+            }
+
+            string url = "/unidad/ListarUnidades/" + unidadModificado.IdConsorcio;
+            return Redirect(url);
+        }
+
+        public ActionResult EliminarUnidad(int id)
+        {
+            if (Session["IdUsuario"] != null)
+            {
+                Unidad unidadAEliminar = unidad.BuscarUnidad(id);
+
+                var node = SiteMaps.Current.CurrentNode;
+                if (node != null && node.ParentNode != null)
+                {
+                    node.ParentNode.Title = "Consorcio \"" + unidadAEliminar.Nombre + "\"";
+                }
+
+                Consorcio consorcioBuscado = consorcio.BuscarConsorcio(unidadAEliminar.IdConsorcio);
+                ViewBag.NombreConsorcio = consorcioBuscado.Nombre;
+                return View(unidadAEliminar);
+            }
+            else
+            {
+                TempData["Controlador"] = "Unidad";
+                TempData["Accion"] = "EliminarUnidad";
+                return RedirectToAction("Ingresar", "Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DarDeBajaUnidad(int id)
+        {
+            string url = "/unidad/ListarUnidades/" + unidad.BuscarUnidad(id).IdConsorcio;
+            unidad.EliminarUnidad(id);
+            return Redirect(url);
+        }
+    }
+}
