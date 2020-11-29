80e6b20c3abd57fefa7c2a25ccd523765222a03b
Gallo <g>
2020-11-28 21:57:59 -0300

    Fix validación de monto decimal en el lado del cliente

--- WebApp/Scripts/jquery.validate.unobtrusive.js
+++ WebApp/Scripts/jquery.validate.unobtrusive.js
@@ -430,3 +430,7 @@
 
     return $jQval.unobtrusive;
 }));
+
+$.validator.methods.number = function (value, element) {
+    return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)?(?:,\d+)?$/.test(value);
+}
